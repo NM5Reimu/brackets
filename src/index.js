0cb6f1514ab9bcec990df15271a167c37270c1a6
Reimu <nm5reimu@gmail.com>
2020-10-09 18:07:18 +0300

    feat: added task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 === 1) return false;
+  
+  let temp = str.split('');
+  let result = [];
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (temp[i] === bracketsConfig[j][1] && result[result.length - 1] === bracketsConfig[j][0]) {
+        result.pop();
+      } else if (temp[i] === bracketsConfig[j][0]) {
+        result.push(temp[i]);
+      }
+    }
+  }
+
+  return result.length === 0;
 }
